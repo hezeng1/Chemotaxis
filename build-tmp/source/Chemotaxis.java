49f0aad95ed5ab46fe7ab3b2d97e4093259f3bbf
hezeng1 <herman.zeng114@gmail.com>
2015-09-18 11:34:49 -0700

    movement added

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,41 +15,46 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
  Bacteria [] colony;
+ int x = (int)(Math.random()*401);
+ int y = (int)(Math.random()*401);
+
  public void setup()   
  {     
  	size(400,400);
  	colony = new Bacteria[100];
+ 	for(int p = 0; p<colony.length;p++)
+ 	{
+ 		colony[p] = new Bacteria(x,y);
+ 	}
 }   
  public void draw()   
- {    
-   
-    background(0);
-    fill((int)(Math.random()*256));
-    for(int i = 0; i<colony.length;i++)
+ { 
+   background(255);
+   for(int i = 0; i<colony.length;i++)
     {
-    	colony[i] = new Bacteria(50,50);
     	colony[i].move();
     	colony[i].show();
      }	
  }  
  class Bacteria    
  {     
- 	int myX,myY;
-    Bacteria(int x , int y )
+ 	int myX,myY,myColor;
+    Bacteria(int x , int y)
  	{
- 		myX = 0;
- 		myY = 0;
+ 		myX = x;
+ 		myY = y;
+ 		myColor = color(13,178,35);
  	}
  	public void move()
  	{
- 	   myX = myX+(int)(Math.random()*20)-10;
- 	   myY = myY+(int)(Math.random()*20)-10;
+ 	   myX = myX+(int)(Math.random()*21)-10;
+ 	   myY = myY+(int)(Math.random()*21)-10;
          
  	} 
  	public void show()
  	{
-       
-       ellipse(myX,myY,10,10);
+       fill(myColor);
+       ellipse(myX,myY,5,5);
  	}  
  }    
   static public void main(String[] passedArgs) {
